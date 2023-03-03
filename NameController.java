691552c36a5490c1d1ed13b071f5025d5ae14eb2
elephant_seal <mikan.ha.orange123@gmail.com>
2023-03-03 23:37:19 +0900

    1st init

---
+++ NameController.java
@@ -0,0 +1,37 @@
+package com.example.RestAPI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Map;
+
+@RestController
+public class NameController {
+
+    @GetMapping("/names")
+    public String getNames(@RequestParam("name") String name){
+        return "Hello " + name + "さん";
+    }
+
+    @PostMapping("/names")
+    public ResponseEntity<String> create(@RequestBody CreateForm form){
+        URI url = UriComponentsBuilder.fromUriString("http://localhost:8080")
+                .path("/names/id")
+                .build()
+                .toUri();
+        return ResponseEntity.created(url).body("name successfully created");
+    }
+
+    @PatchMapping("/names/{id}")
+    public ResponseEntity<Map<String, String>> update(@PathVariable("id") int id, @RequestBody UpdateForm form){
+        return ResponseEntity.ok(Map.of("message", "name successfully updated"));
+    }
+
+    @DeleteMapping("/names/{id}")
+    public ResponseEntity<Map<String, String>> delete(@PathVariable("id") int id){
+        return ResponseEntity.ok(Map.of("message", "name successfully deleted"));
+    }
+
+}
